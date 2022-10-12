89801d05e2707317afa98ff2e53d4257358f0d84
skonhwang <seungkon.hwang@dsrvlabs.com>
2022-10-12 08:48:55 +0000

    peer_count: Remove hostname

--- plugins/peer_count/main.go
+++ plugins/peer_count/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"strconv"
-	"os"
 
 	rpcNetInfo "github.com/dsrvlabs/vatz-plugin-cosmoshub/rpc/cosmos"
 	pluginpb "github.com/dsrvlabs/vatz-proto/plugin/v1"
@@ -52,26 +51,22 @@ func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, e
 	state := pluginpb.STATE_NONE
 
 	var msg string
-	var hostname string
 
 	peers, err := rpcNetInfo.GetNpeers()
 	if err == nil {
 		npeer, _ := strconv.Atoi(peers)
 		if npeer < minPeer {
 			severity = pluginpb.SEVERITY_CRITICAL
-			hostname, _ = os.Hostname()
-			msg = fmt.Sprintf("[%s]Bad: peer_count is %d", hostname, npeer)
+			msg = fmt.Sprintf("Bad: peer_count is %d", npeer)
 			log.Info().Str("moudle", "plugin").Msg(msg)
 		} else {
 			state = pluginpb.STATE_SUCCESS
-			hostname, _ = os.Hostname()
-			msg = fmt.Sprintf("[%s]Good: peer_count is %d", hostname, npeer)
+			msg = fmt.Sprintf("Good: peer_count is %d", npeer)
 			log.Info().Str("moudle", "plugin").Msg(msg)
 		}
 	} else {
 		severity = pluginpb.SEVERITY_ERROR
-		hostname, _ = os.Hostname()
-		msg = fmt.Sprintf("[%s]Error to get #N peers", hostname)
+		msg = "[%s]Error to get #N peers"
 		log.Info().Str("moudle", "plugin").Msg(msg)
 	}
 
