42b978bb177e947a2d019a6774d5ba2ee23a7d84
Jinhong Choi <jinhongdev@gmail.com>
2022-11-08 14:20:21 +0900

    Add test code for net_info

---
+++ rpc/cosmos/net_info_test.go
@@ -0,0 +1,25 @@
+package rpc
+
+import (
+	"net/http"
+	"strconv"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetNpeers(t *testing.T) {
+	httpmock.Activate()
+
+	httpmock.RegisterResponder(
+		http.MethodGet,
+		"https://agoric-mainnet-rpc.allthatnode.com:26657/net_info",
+		httpmock.NewStringResponder(http.StatusOK, fixturePeerCount),
+	)
+
+	ret, err := GetNpeers()
+	assert.Nil(t, err)
+	peerCount, _ := strconv.Atoi(ret)
+	assert.GreaterOrEqual(t, peerCount, 0)
+}
