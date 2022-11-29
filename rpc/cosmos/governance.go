4c8376d2ae49ed248180b42629e61af68cd0df60
skonhwang <seungkon.hwang@dsrvlabs.com>
2022-11-29 12:06:09 +0000

    This patch supports for governance alarm

---
+++ rpc/cosmos/governance.go
@@ -0,0 +1,76 @@
+package rpc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type Governance struct {
+	Proposal struct {
+		ProposalID string `json:"proposal_id"`
+		Content    struct {
+			Type                string      `json:"@type"`
+			Title               string      `json:"title"`
+			Description         string      `json:"description"`
+			Ticker              string      `json:"ticker"`
+			BaseDenom           string      `json:"base_denom"`
+			QuoteDenom          string      `json:"quote_denom"`
+			MinPriceTickSize    string      `json:"min_price_tick_size"`
+			MinQuantityTickSize string      `json:"min_quantity_tick_size"`
+			MakerFeeRate        interface{} `json:"maker_fee_rate"`
+			TakerFeeRate        interface{} `json:"taker_fee_rate"`
+		} `json:"content"`
+		Status           string `json:"status"`
+		FinalTallyResult struct {
+			Yes        string `json:"yes"`
+			Abstain    string `json:"abstain"`
+			No         string `json:"no"`
+			NoWithVeto string `json:"no_with_veto"`
+		} `json:"final_tally_result"`
+		SubmitTime     time.Time `json:"submit_time"`
+		DepositEndTime time.Time `json:"deposit_end_time"`
+		TotalDeposit   []struct {
+			Denom  string `json:"denom"`
+			Amount string `json:"amount"`
+		} `json:"total_deposit"`
+		VotingStartTime time.Time `json:"voting_start_time"`
+		VotingEndTime   time.Time `json:"voting_end_time"`
+	} `json:"proposal"`
+}
+
+func GetProposal(apiPort uint, prop uint) (string, error) {
+	url := fmt.Sprintf("http://localhost:%d/cosmos/gov/v1beta1/proposal/%d", apiPort, prop)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	rawBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return strconv.Itoa(resp.StatusCode), fmt.Errorf("request failed %s", string(rawBody))
+	}
+
+	gov := Governance{}
+	err = json.Unmarshal(rawBody, &gov)
+
+	if err != nil {
+		return err.Error(), err
+	}
+
+	return gov.Proposal.ProposalID, nil
+}
