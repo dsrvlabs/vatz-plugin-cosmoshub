338733359c266d8a7a691cfec07a270cab085b9f
skonhwang <seungkon.hwang@dsrvlabs.com>
2022-12-06 04:55:25 +0000

    This patch supports to check the answer of proposal

--- plugins/node_governance_alarm/main.go
+++ plugins/node_governance_alarm/main.go
@@ -21,6 +21,7 @@ const (
 	defaultPeer = 5
 	defaultApiPort = 1317
 	defaultProposalId = 0
+	defaultVoterVote = "address"
 	iterCnt = 100
 
 	pluginName = "node-governance-alarm"
@@ -31,6 +32,8 @@ var (
 	port		int
 	apiPort		uint
 	proposalId	uint
+	voterAddr	string
+	firstRun	int
 )
 
 func init() {
@@ -38,10 +41,41 @@ func init() {
 	flag.IntVar(&port, "port", defaultPort, "Port number, default 9091")
 	flag.UintVar(&apiPort, "apiPort", defaultApiPort, "Need to know proposal id")
 	flag.UintVar(&proposalId, "proposalId", defaultProposalId, "Need to know last proposal id")
+	flag.StringVar(&voterAddr, "voterAddr", defaultVoterVote, "Need to voter address")
 
 	flag.Parse()
 
 	findLastestProposalId()
+	firstRun = 0
+}
+
+func CheckVoterVote() string {
+	tmpCnt := iterCnt
+	var needToVote string
+	for j := proposalId; ; j-- {
+		_, t, _ := rpcGovernance.GetProposal(apiPort, uint(j))
+		if j == 0 || tmpCnt == 0 || t.Before(time.Now()) {
+			break
+		}
+		tmpCnt--
+		vote, _ := rpcGovernance.GetVoterVote(apiPort, uint(j), voterAddr)
+		var myVote string
+		if vote == "VOTE_OPTION_YES" {
+			myVote = "Yes"
+		} else if vote == "VOTE_OPTION_NO" {
+			myVote = "No"
+		} else if vote == "VOTE_OPTION_ABSTAIN" {
+			myVote = "Abstain"
+		} else if vote == "VOTE_OPTION_NO_WITH_VETO" {
+			myVote = "NoWithVeto"
+		} else {
+			myVote = "Dit not vote"
+		}
+		voteMsg := fmt.Sprintf("Proposal %d: %s", j, myVote)
+		needToVote += voteMsg + "\n"
+		log.Info().Str("module", "plugin").Msg(voteMsg)
+	}
+	return needToVote
 }
 
 func findLastestProposalId() {
@@ -54,6 +88,7 @@ func findLastestProposalId() {
 	for i := 1; ; i++ {
 		prop, t, err := rpcGovernance.GetProposal(apiPort, uint(i))
 		if err == nil && t.Year() > time.Now().Year() / 2 {
+		//if err == nil && t.After(time.Now()) {
 			num_prop, _ := strconv.Atoi(prop)
 			msg += fmt.Sprintf("New proposal : %d, %d-%02d-%02d\n", num_prop, t.Year(), t.Month(), t.Day())
 			proposalId = uint(i)
@@ -66,7 +101,9 @@ func findLastestProposalId() {
 			}
 		}
 	}
+
 	log.Info().Str("module", "plugin").Msg(msg)
+	CheckVoterVote()
 }
 
 func main() {
@@ -85,28 +122,39 @@ func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, e
 
 	var msg string
 	tmp := proposalId
-	fmt.Println("Debug: last proposal = ", proposalId)
 
 	for i := 1; i <= iterCnt; i++ {
 		prop, t, err := rpcGovernance.GetProposal(apiPort, tmp + uint(i))
-		if err == nil && t.Year() > time.Now().Year() / 2 {
+		//if err == nil && t.Year() > time.Now().Year() / 2 {
+		if err == nil && t.After(time.Now()) {
 			num_prop, _ := strconv.Atoi(prop)
 			msg += fmt.Sprintf("New proposal : %d, %d-%02d-%02d\n", num_prop, t.Year(), t.Month(), t.Day())
 			proposalId = tmp + uint(i)
 		} else {
 			if i == iterCnt {
-				msg += fmt.Sprintf("Lastest proposal is #%d", proposalId)
+				msg += fmt.Sprintf("Lastest proposal is #%d\n", proposalId)
 			}
 		}
 	}
 	if tmp == proposalId {
-		fmt.Println("DEBUG : tmp == proposalId")
+		log.Debug().Str("module", "plugin").Msg("DEBUG : tmp == proposalId")
 		severity = pluginpb.SEVERITY_INFO
 		state = pluginpb.STATE_SUCCESS
+		list := CheckVoterVote()
+		if len(list) != 0 && firstRun == 0 {
+			severity = pluginpb.SEVERITY_CRITICAL
+			state = pluginpb.STATE_SUCCESS
+			msg += list
+			firstRun++
+		}
 	} else {
-		fmt.Println("DEBUG : tmp != proposalId")
+		log.Debug().Str("module", "plugin").Msg("DEBUG : tmp != proposalId")
 		severity = pluginpb.SEVERITY_CRITICAL
 		state = pluginpb.STATE_SUCCESS
+		list := CheckVoterVote()
+		if len(list) != 0 {
+			msg += list
+		}
 	}
 	log.Info().Str("module", "plugin").Msg(msg)
 	ret := sdk.CallResponse{
--- rpc/cosmos/governance.go
+++ rpc/cosmos/governance.go
@@ -42,6 +42,18 @@ type Governance struct {
 	} `json:"proposal"`
 }
 
+type VoterVote struct {
+	Vote struct {
+		ProposalID string `json:"proposal_id"`
+		Voter      string `json:"voter"`
+		Option     string `json:"option"`
+		Options    []struct {
+			Option string `json:"option"`
+			Weight string `json:"weight"`
+		} `json:"options"`
+	} `json:"vote"`
+}
+
 func GetProposal(apiPort uint, prop uint) (string, time.Time, error) {
 	url := fmt.Sprintf("http://localhost:%d/cosmos/gov/v1beta1/proposals/%d", apiPort, prop)
 	//url := fmt.Sprintf("https://api.cosmos.network/cosmos/gov/v1beta1/proposals/%d", prop)
@@ -74,5 +86,39 @@ func GetProposal(apiPort uint, prop uint) (string, time.Time, error) {
 		return err.Error(), time.Now(), err
 	}
 
-	return gov.Proposal.ProposalID, gov.Proposal.VotingStartTime, nil
+	return gov.Proposal.ProposalID, gov.Proposal.VotingEndTime, nil
+}
+
+func GetVoterVote(apiPort uint, prop uint, voterAddr string) (string, error) {
+	url := fmt.Sprintf("http://localhost:%d/cosmos/gov/v1beta1/proposals/%d/votes/%s", apiPort, prop, voterAddr)
+	//fmt.Println(url)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	rawBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return strconv.Itoa(resp.StatusCode), fmt.Errorf("request failed %s", string(rawBody))
+	}
+
+	vote := VoterVote{}
+	err = json.Unmarshal(rawBody, &vote)
+
+	if err != nil {
+		return err.Error(), err
+	}
+
+	return vote.Vote.Option, nil
 }
