7f259d3bdffedd76f49a3382b5ef4efdd341ad73
Hwangjae Lee <meetrick@gmail.com>
2023-09-20 15:33:45 +0900

    Modified net_info api
    Signed-off-by: Hwangjae Lee <meetrick@gmail.com>

--- rpc/cosmos/net_info.go
+++ rpc/cosmos/net_info.go
@@ -12,72 +12,72 @@ import (
 type netInfo struct {
 	Jsonrpc string `json:"jsonrpc"`
 	ID      int    `json:"id"`
-	Result  struct {
-		Listening bool     `json:"listening"`
-		Listeners []string `json:"listeners"`
-		NPeers    string   `json:"n_peers"`
-		Peers     []struct {
-			NodeInfo struct {
-				ProtocolVersion struct {
-					P2P   string `json:"p2p"`
-					Block string `json:"block"`
-					App   string `json:"app"`
-				} `json:"protocol_version"`
-				ID         string `json:"id"`
-				ListenAddr string `json:"listen_addr"`
-				Network    string `json:"network"`
-				Version    string `json:"version"`
-				Channels   string `json:"channels"`
-				Moniker    string `json:"moniker"`
-				Other      struct {
-					TxIndex    string `json:"tx_index"`
-					RPCAddress string `json:"rpc_address"`
-				} `json:"other"`
-			} `json:"node_info"`
-			IsOutbound       bool `json:"is_outbound"`
-			ConnectionStatus struct {
-				Duration    string `json:"Duration"`
-				SendMonitor struct {
-					Start    time.Time `json:"Start"`
-					Bytes    string    `json:"Bytes"`
-					Samples  string    `json:"Samples"`
-					InstRate string    `json:"InstRate"`
-					CurRate  string    `json:"CurRate"`
-					AvgRate  string    `json:"AvgRate"`
-					PeakRate string    `json:"PeakRate"`
-					BytesRem string    `json:"BytesRem"`
-					Duration string    `json:"Duration"`
-					Idle     string    `json:"Idle"`
-					TimeRem  string    `json:"TimeRem"`
-					Progress int       `json:"Progress"`
-					Active   bool      `json:"Active"`
-				} `json:"SendMonitor"`
-				RecvMonitor struct {
-					Start    time.Time `json:"Start"`
-					Bytes    string    `json:"Bytes"`
-					Samples  string    `json:"Samples"`
-					InstRate string    `json:"InstRate"`
-					CurRate  string    `json:"CurRate"`
-					AvgRate  string    `json:"AvgRate"`
-					PeakRate string    `json:"PeakRate"`
-					BytesRem string    `json:"BytesRem"`
-					Duration string    `json:"Duration"`
-					Idle     string    `json:"Idle"`
-					TimeRem  string    `json:"TimeRem"`
-					Progress int       `json:"Progress"`
-					Active   bool      `json:"Active"`
-				} `json:"RecvMonitor"`
-				Channels []struct {
-					ID                int    `json:"ID"`
-					SendQueueCapacity string `json:"SendQueueCapacity"`
-					SendQueueSize     string `json:"SendQueueSize"`
-					Priority          string `json:"Priority"`
-					RecentlySent      string `json:"RecentlySent"`
-				} `json:"Channels"`
-			} `json:"connection_status"`
-			RemoteIP string `json:"remote_ip"`
-		} `json:"peers"`
-	} `json:"result"`
+	//	Result  struct {
+	Listening bool     `json:"listening"`
+	Listeners []string `json:"listeners"`
+	NPeers    string   `json:"n_peers"`
+	Peers     []struct {
+		NodeInfo struct {
+			ProtocolVersion struct {
+				P2P   string `json:"p2p"`
+				Block string `json:"block"`
+				App   string `json:"app"`
+			} `json:"protocol_version"`
+			ID         string `json:"id"`
+			ListenAddr string `json:"listen_addr"`
+			Network    string `json:"network"`
+			Version    string `json:"version"`
+			Channels   string `json:"channels"`
+			Moniker    string `json:"moniker"`
+			Other      struct {
+				TxIndex    string `json:"tx_index"`
+				RPCAddress string `json:"rpc_address"`
+			} `json:"other"`
+		} `json:"node_info"`
+		IsOutbound       bool `json:"is_outbound"`
+		ConnectionStatus struct {
+			Duration    string `json:"Duration"`
+			SendMonitor struct {
+				Start    time.Time `json:"Start"`
+				Bytes    string    `json:"Bytes"`
+				Samples  string    `json:"Samples"`
+				InstRate string    `json:"InstRate"`
+				CurRate  string    `json:"CurRate"`
+				AvgRate  string    `json:"AvgRate"`
+				PeakRate string    `json:"PeakRate"`
+				BytesRem string    `json:"BytesRem"`
+				Duration string    `json:"Duration"`
+				Idle     string    `json:"Idle"`
+				TimeRem  string    `json:"TimeRem"`
+				Progress int       `json:"Progress"`
+				Active   bool      `json:"Active"`
+			} `json:"SendMonitor"`
+			RecvMonitor struct {
+				Start    time.Time `json:"Start"`
+				Bytes    string    `json:"Bytes"`
+				Samples  string    `json:"Samples"`
+				InstRate string    `json:"InstRate"`
+				CurRate  string    `json:"CurRate"`
+				AvgRate  string    `json:"AvgRate"`
+				PeakRate string    `json:"PeakRate"`
+				BytesRem string    `json:"BytesRem"`
+				Duration string    `json:"Duration"`
+				Idle     string    `json:"Idle"`
+				TimeRem  string    `json:"TimeRem"`
+				Progress int       `json:"Progress"`
+				Active   bool      `json:"Active"`
+			} `json:"RecvMonitor"`
+			Channels []struct {
+				ID                int    `json:"ID"`
+				SendQueueCapacity string `json:"SendQueueCapacity"`
+				SendQueueSize     string `json:"SendQueueSize"`
+				Priority          string `json:"Priority"`
+				RecentlySent      string `json:"RecentlySent"`
+			} `json:"Channels"`
+		} `json:"connection_status"`
+		RemoteIP string `json:"remote_ip"`
+	} `json:"peers"`
+	//	} `json:"result"`
 }
 
 func GetNpeers(rpcAddr string) (string, error) {
